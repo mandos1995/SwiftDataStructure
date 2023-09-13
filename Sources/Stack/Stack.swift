e7d779a98ee7ea7da4e9e2ed5dea57e9dd918c38
mandos1995 <mandos19950620@gmail.com>
2023-09-13 14:24:06 +0900

    chore: resetting git

---
+++ Sources/Stack/Stack.swift
@@ -0,0 +1,35 @@
+//
+//  Stack.swift
+//  
+//
+//  Created by 김민석 on 2023/09/13.
+//
+
+public struct Stack<T> {
+    
+    public init() { }
+    
+    private var array: [T] = []
+    
+    public var top: T? {
+        return array.last
+    }
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    mutating public func push(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating public func pop() -> T? {
+        return array.popLast()
+    }
+}
+
+
